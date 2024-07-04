a937f63277517cdcef23c713fb7edd507776d461
tomtjes <nesiofnqwe@mailbox.org>
2024-07-04 12:15:21 -0400

    test US Soundboard

---
+++ Third Party/tomtjes_Install Ultraschall Soundboard.lua
@@ -0,0 +1,21 @@
+--[[
+    Name:
+        Install Ultraschall Soundboard
+    Screenshot:
+    Author:
+        tomtjes
+    Donation:
+    Links:
+    Version:
+        1.0-test1
+    MetaPackage: true
+    Provides: 
+        [effect] https://github.com/Ultraschall/ultraschall-portable/raw/master/UserPlugins/FX/Soundboard.vst3
+        [tracktpl] https://github.com/Ultraschall/ultraschall-portable/raw/master/TrackTemplates/Insert%20StudioLink%20track.RTrackTemplate
+    Changelog:
+        + initial release
+    License:
+        GPL v3
+    About:
+        # Install Ultraschall Soundboard
+--]]
